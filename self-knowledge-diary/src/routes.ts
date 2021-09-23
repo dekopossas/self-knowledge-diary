0538c89179410d3b056f418c33a05030ed80a59a
deko possas <andrepossaster@gmail.com>
2021-09-23 20:24:24 -0300

    new file days

--- self-knowledge-diary/src/routes.ts
+++ self-knowledge-diary/src/routes.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
-  return response.json({ message: 'hello world!' });
+  return response.json({ message: 'hello world3!' });
 });
 
 export default routes;
