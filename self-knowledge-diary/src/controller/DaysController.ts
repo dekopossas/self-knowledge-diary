bab9be1b3387b26d59f3003893e81b4eeecfe099
deko possas <andrepossaster@gmail.com>
2021-09-24 20:26:20 -0300

    updateDay controlle done

--- self-knowledge-diary/src/controller/DaysController.ts
+++ self-knowledge-diary/src/controller/DaysController.ts
@@ -11,9 +11,20 @@ export const getDay = async (request: Request, response: Response) => {
   const { id } = request.params;
   const day = await getRepository(Days).findOne(id);
   return response.json(day);
-}
+};
 
 export const saveDay = async (request: Request, response: Response) => {
   const days = await getRepository(Days).save(request.body);
   response.json(days);
+};
+
+export const updateDay = async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const day = await getRepository(Days).update(id, request.body);
+  if (day.affected === 1) {
+    const dayUpdated = await getRepository(Days).findOne(id);
+    return response.json(dayUpdated);
+  }
+
+  return response.status(404).json({ message: 'Day not found' });
 };
