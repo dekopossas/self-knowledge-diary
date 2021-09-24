ad2dd086ef4e318b228f997acc106a6788c8b2d5
deko possas <andrepossaster@gmail.com>
2021-09-24 19:56:55 -0300

    new controller router get days

--- self-knowledge-diary/src/routes.ts
+++ self-knowledge-diary/src/routes.ts
@@ -1,9 +1,13 @@
 import { Router, Request, Response } from 'express';
 
+import { getDays } from './controller/DaysController';
+
 const routes = Router();
 
 routes.get('/', (request: Request, response: Response) => {
   return response.json({ message: 'hello world3!' });
 });
+
+routes.get('/days', getDays);
 
 export default routes;
