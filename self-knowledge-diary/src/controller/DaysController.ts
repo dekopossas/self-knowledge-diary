6ac0c3db04e946f1fa9066c8ec29080517858476
deko possas <andrepossaster@gmail.com>
2021-09-23 21:48:53 -0300

    Days controller find

---
+++ self-knowledge-diary/src/controller/DaysController.ts
@@ -0,0 +1,10 @@
+import { getRepository } from 'typeorm';
+import { Days } from '../entity/Days';
+import { Request, Response } from 'express';
+
+export const getDays = async (request: Request, response: Response) => {
+
+  const days = await getRepository(Days).find()
+  return response.json(days)
+
+}
