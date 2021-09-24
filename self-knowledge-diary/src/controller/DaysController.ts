61ac6393be96ed1e090d6f8067dec1289918bcf6
deko possas <andrepossaster@gmail.com>
2021-09-24 19:55:15 -0300

    lint

--- self-knowledge-diary/src/controller/DaysController.ts
+++ self-knowledge-diary/src/controller/DaysController.ts
@@ -3,8 +3,6 @@ import { Days } from '../entity/Days';
 import { Request, Response } from 'express';
 
 export const getDays = async (request: Request, response: Response) => {
-
-  const days = await getRepository(Days).find()
-  return response.json(days)
-
-}
+  const days = await getRepository(Days).find();
+  return response.json(days);
+};
