5730281f9800dd01564daf42baf5776d62142eac
deko possas <andrepossaster@gmail.com>
2021-09-24 20:56:03 -0300

    delete day route

--- self-knowledge-diary/src/routes.ts
+++ self-knowledge-diary/src/routes.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from 'express';
 
-import { getDays, saveDay, getDay, updateDay } from './controller/DaysController';
+import { getDays, saveDay, getDay, updateDay, deleteDay } from './controller/DaysController';
 
 const routes = Router();
 
@@ -12,5 +12,6 @@ routes.get('/days', getDays);
 routes.get('/days/:id', getDay);
 routes.post('/days', saveDay);
 routes.put('/days/:id', updateDay);
+routes.delete('/days/:id', deleteDay);
 
 export default routes;
