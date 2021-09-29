75538f147128caf429d0b5893b65730dbddb28e1
deko possas <andrepossaster@gmail.com>
2021-09-28 22:23:49 -0300

    new route Days

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,13 +1,16 @@
+// Packages
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+// Pages
+import Days from './pages/Days';
 import Home from './pages/Home';
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" component={} />
-      <Route path="/calendario" component={} />
+      <Route path="/" component={Home} />
+      <Route path="/calendario" component={Days} />
     </Switch>
   );
 }
