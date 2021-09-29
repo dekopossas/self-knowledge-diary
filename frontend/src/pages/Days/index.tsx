b405e33746f67bd8bb3f64c6efa04d8b4382bf25
deko possas <andrepossaster@gmail.com>
2021-09-28 22:19:42 -0300

    route file done

---
+++ frontend/src/pages/Days/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Days() {
+  return (
+    <div>
+      <h1>Days Pages</h1>
+    </div>
+  )
+}
+
+export default Days
---
+++ frontend/src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+function Routes() {
+  return (
+    <Switch>
+      <Route path="/" component={} />
+      <Route path="/calendario" component={} />
+    </Switch>
+  );
+}
+
+export default Routes;
