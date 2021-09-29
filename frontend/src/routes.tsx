adf3c08442809da38e1d26f7a7cedfc9291a98e8
deko possas <andrepossaster@gmail.com>
2021-09-28 22:27:53 -0300

    add exact

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 // Pages
-import Days from './pages/Days';
 import Home from './pages/Home';
+import Days from './pages/Days';
 
 function Routes() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/calendario" component={Days} />
+      <Route path="/" exact component={Home} />
+      <Route path="/calendario" exact component={Days} />
     </Switch>
   );
 }
