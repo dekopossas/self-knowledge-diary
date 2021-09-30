d7559d83ef66f9f0f523a69778cafd125f95f306
deko possas <andrepossaster@gmail.com>
2021-09-29 21:11:56 -0300

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import * as express from 'express';
+import * as cors from 'cors';
 import routes from './routes';
 
 const app = express();
 createConnection();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
