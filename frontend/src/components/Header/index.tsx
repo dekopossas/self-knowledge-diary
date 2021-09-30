a85e24f9a2f8fef8e66d1cb4acbf2151c9a0ad60
deko possas <andrepossaster@gmail.com>
2021-09-29 21:03:20 -0300

    href done

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,8 +9,8 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Cadastrar Dia</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/calendario">Cadastrar Dia</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
