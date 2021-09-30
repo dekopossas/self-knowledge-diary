767a58878611c02903d5336bcd91115ac09bc90c
deko possas <andrepossaster@gmail.com>
2021-09-30 19:54:52 -0300

    lint and some removes

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,25 +1,10 @@
 import React, { useEffect } from 'react';
-import { useHistory, Link } from 'react-router-dom';
 
 function Home() {
-  const history = useHistory();
-
-  useEffect(() => {
-    console.log('Renderizou');
-  }, []);
 
   return (
     <div>
-      <h1>Home page {history.location.search} </h1>
-      <Link to="/?deko=possas">
-        <button>testando Link</button>
-      </Link>
-      <Link to="/?deko=possas2">
-        <button>testando Link</button>
-      </Link>
-      <Link to="/?deko=possas3">
-        <button>testando Link</button>
-      </Link>
+      <h1>Home page</h1>
     </div>
   );
 }
