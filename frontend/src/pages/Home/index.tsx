4686974bd953dd7562ef5f3ed89ef17fd7cfca0a
deko possas <andrepossaster@gmail.com>
2021-09-30 19:43:56 -0300

    reset commit

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 
 function Home() {
+  const history = useHistory();
+
+  useEffect(() => {
+    console.log('Renderizou');
+  }, []);
+
   return (
     <div>
-      home page
+      <h1>Home page {history.location.search} </h1>
+      <Link to="/?deko=possas">
+        <button>testando Link</button>
+      </Link>
+      <Link to="/?deko=possas2">
+        <button>testando Link</button>
+      </Link>
+      <Link to="/?deko=possas3">
+        <button>testando Link</button>
+      </Link>
     </div>
-  )
+  );
 }
 
 export default Home;
