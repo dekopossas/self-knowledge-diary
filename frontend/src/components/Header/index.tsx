abce7a463ffca349c63750e21c39b1ef050768e2
deko possas <andrepossaster@gmail.com>
2021-09-29 21:08:59 -0300

    underfull style of header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Navbar.Brand href="#home">Seja uma pessoa melhor.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/calendario">Cadastrar Dia</Nav.Link>
+            <Nav.Item as={Link} className="nav-link" to="/" >Home</Nav.Item>
+            <Nav.Item as={Link} className="nav-link" to="/calendario" >Diário</Nav.Item>
           </Nav>
         </Navbar.Collapse>
       </Container>
