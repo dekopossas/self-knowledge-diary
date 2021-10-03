c329856254f1a1aaa772e53f3b97f5884c2d4ee2
deko possas <andrepossaster@gmail.com>
2021-10-03 20:27:00 -0300

    form done

--- frontend/src/pages/Days/FormDay/index.tsx
+++ frontend/src/pages/Days/FormDay/index.tsx
@@ -54,16 +54,20 @@ function FormDay() {
             <Form.Check value={4} inline name="feeling" type="radio" label="Nojo" />
             <Form.Check value={5} inline name="feeling" type="radio" label="Tristeza" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Sentimento</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+          <Form.Group className="mb-3">
+            <Form.Label>Gatilho:</Form.Label>
+            <Form.Control type="text" />
           </Form.Group>
 
           <Form.Group className="mb-3">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label>Como reagil:</Form.Label>
+            <Form.Control type="text" />
           </Form.Group>
 
+          <Form.Group className="mb-3">
+            <Form.Label>Como poderia ter reagido:</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>
