6aa8b9fec016c3c41975d237ae8bf9adca5ef711
deko possas <andrepossaster@gmail.com>
2021-10-03 19:02:18 -0300

    new routes created

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,12 +5,14 @@ import { Switch, Route } from 'react-router-dom';
 // Pages
 import Home from './pages/Home';
 import Days from './pages/Days';
+import FormDay from './pages/Days/FormDay';
 
 function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/calendario" exact component={Days} />
+      <Route path="/calendario/nova-observacao" exact component={FormDay} />
     </Switch>
   );
 }
