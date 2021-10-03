4a7f1a3bdbf7958ceeb8c8c1721ddd77a2c4a0e0
deko possas <andrepossaster@gmail.com>
2021-10-03 19:11:59 -0300

    form copy and past of react bootstrap

--- frontend/src/pages/Days/FormDay/index.tsx
+++ frontend/src/pages/Days/FormDay/index.tsx
@@ -7,7 +7,7 @@ import api from '../../../services/api';
 // Lib
 
 // Style
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import styles from './style.module.scss';
 
 interface IDay {
@@ -25,11 +25,9 @@ function FormDay() {
   // States
   // ----------------------------------------------------------------
 
-
   // ----------------------------------------------------------------
   // Functions
   // ----------------------------------------------------------------
-
 
   // ----------------------------------------------------------------
   // Effects
@@ -40,9 +38,33 @@ function FormDay() {
       <br />
       <div className={styles.day_header}>
         <h2>Nova Obrservação</h2>
-        <Button variant="dark" size="sm">Voltar</Button>
+        <Button variant="dark" size="sm">
+          Voltar
+        </Button>
       </div>
       <br />
+      <div className="container">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
